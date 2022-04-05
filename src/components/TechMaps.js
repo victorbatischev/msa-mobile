cda65b62d0c189a3c131e72d27001765dede745f
Maksim <boryskatowsky.gmail.com>
2022-04-05 17:51:58 +0300

    add video and pdf

--- src/components/TechMaps.js
+++ src/components/TechMaps.js
@@ -11,61 +11,65 @@ import {
 } from 'react-native'
 import AppIntroSlider from 'react-native-app-intro-slider'
 import ImageZoom from 'react-native-image-pan-zoom'
+import { Video } from 'expo-av'
+import * as WebBrowser from 'expo-web-browser'
 
 const TechMaps = ({ operationId }) => {
   const [mapsArr, setMapsArr] = useState(null)
   const [modalVisible, setModalVisible] = useState(false)
+  const [item, setItem] = useState(null)
   useEffect(() => {
     axios.get(`order_worker_techmap/${operationId}`).then((response) => {
       setMapsArr(response.data[0].technical_maps)
     })
   }, [])
+
   const renderItem = ({ item }) => {
     return (
-      <Pressable style={styles.container} onPress={() => setModalVisible(true)}>
+      <View style={styles.container}>
         <Text style={styles.mapName}>{item.name}</Text>
-        <Image
-          source={{ uri: item.file_url }}
-          style={{ height: '80%' }}
-          resizeMode={'contain'}
-        />
-        <Modal visible={modalVisible} transparent={false}>
+        {item.file_name.split('.').pop() == 'jpg' && (
           <Pressable
-            style={styles.closeModalButton}
             onPress={() => {
-              setModalVisible(false)
+              setItem(item)
+              setModalVisible(true)
             }}
-          >
-            <View
-              style={[
-                styles.closeModalButtonLeftLine,
-                styles.closeModalButtonLine
-              ]}
-            />
-            <View
-              style={[
-                styles.closeModalButtonRightLine,
-                styles.closeModalButtonLine
-              ]}
-            />
-          </Pressable>
-          <ImageZoom
-            cropWidth={Dimensions.get('window').width}
-            cropHeight={Dimensions.get('window').height}
-            imageWidth={Dimensions.get('window').width}
-            imageHeight={Dimensions.get('window').width / 1.43}
           >
             <Image
               source={{ uri: item.file_url }}
-              style={{
-                height: Dimensions.get('window').width / 1.43,
-                width: Dimensions.get('window').width
-              }}
+              style={{ height: '93%' }}
               resizeMode={'contain'}
             />
-          </ImageZoom>
-        </Modal>
-      </Pressable>
+          </Pressable>
+        )}
+        {item.file_name.split('.').pop() == 'pdf' && (
+          <Pressable
+            onPress={async () => {
+              await WebBrowser.openBrowserAsync(item.file_url)
+            }}
+          >
+            <Image
+              source={require('../assets/icons/pdf.png')}
+              style={{ height: '93%', alignSelf: 'center' }}
+              resizeMode={'contain'}
+            />
+          </Pressable>
+        )}
+        {item.file_name.split('.').pop() == 'm4v' && (
+          <Pressable
+            onPress={() => {
+              setItem(item)
+              setModalVisible(true)
+            }}
+          >
+            <Image
+              source={require('../assets/icons/video.png')}
+              style={{ height: '93%', alignSelf: 'center' }}
+              resizeMode={'contain'}
+            />
+          </Pressable>
+        )}
+      </View>
     )
   }
   const renderNextButton = () => {
@@ -100,6 +104,54 @@ const TechMaps = ({ operationId }) => {
       ) : (
         <Text>loading...</Text>
       )}
+      <Modal visible={modalVisible} transparent={false}>
+        <Pressable
+          style={styles.closeModalButton}
+          onPress={() => {
+            setModalVisible(false)
+          }}
+        >
+          <View
+            style={[
+              styles.closeModalButtonLeftLine,
+              styles.closeModalButtonLine
+            ]}
+          />
+          <View
+            style={[
+              styles.closeModalButtonRightLine,
+              styles.closeModalButtonLine
+            ]}
+          />
+        </Pressable>
+        {item?.file_name.split('.').pop() == 'jpg' && (
+          <ImageZoom
+            cropWidth={Dimensions.get('window').width}
+            cropHeight={Dimensions.get('window').height}
+            imageWidth={Dimensions.get('window').width}
+            imageHeight={Dimensions.get('window').width / 1.43}
+          >
+            <Image
+              source={{ uri: item?.file_url }}
+              style={{
+                height: Dimensions.get('window').width / 1.43,
+                width: Dimensions.get('window').width
+              }}
+              resizeMode={'contain'}
+            />
+          </ImageZoom>
+        )}
+        {item?.file_name.split('.').pop() == 'pdf' && <Text>PDF</Text>}
+        {item?.file_name.split('.').pop() == 'm4v' && (
+          <Video
+            style={{ height: '80%' }}
+            source={{ uri: item.file_url }}
+            resizeMode='contain'
+            useNativeControls={true}
+            shouldPlay={true}
+          />
+        )}
+      </Modal>
     </View>
   )
 }
