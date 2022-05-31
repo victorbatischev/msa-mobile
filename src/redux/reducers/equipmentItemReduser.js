cfb40597516de55686eded84f5a857685dce4779
Maksim <boryskatowsky.gmail.com>
2022-05-31 14:53:05 +0300

    fixed equipmentItemReduser

--- src/redux/reducers/equipmentItemReduser.js
+++ src/redux/reducers/equipmentItemReduser.js
@@ -4,11 +4,7 @@ const initialState = {
   isChecked: []
 }
 
-<<<<<<< HEAD:src/redux/reducers/equipmentItemReduser.js
-export default equipmentItemReduser = (state = initialState, action) => {
-=======
 export default equipmentItemReducer = (state = initialState, action) => {
->>>>>>> 12326c1ce81df15a7278f91fb3f1371652a3afb7:src/redux/reducers/equipmentItemReducer.js
   switch (action.type) {
     case SET_IS_CHECKED_ARR:
       let arr = state.isChecked
