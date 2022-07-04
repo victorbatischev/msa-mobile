a7a725d0ef5aadfa8ba19331075998ecab59993a
Maksim <boryskatowsky.gmail.com>
2022-07-04 17:24:04 +0300

    add setInterval for equipment request

--- src/components/Equipment/Equipment.js
+++ src/components/Equipment/Equipment.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ScrollView, View, Text, Image, Pressable } from 'react-native'
 import { useSelector } from 'react-redux'
 import EquipmentItem from '../EquipmentItem/EquipmentItem'
 import styles from './styles'
 
 const Equipment = ({ equipmentRequest }) => {
+  useEffect(() => {
+    const equipmentRequestInterval = setInterval(() => {
+      equipmentRequest(o_id)
+    }, 5000)
+    return () => clearInterval(equipmentRequestInterval)
+  }, [])
+
   const equipmentArr = useSelector((state) => state.main.equipmentArr)
   const o_id = useSelector((state) => state.main.activeOrder.description.o_id)
   const buttonHandler = () => {
